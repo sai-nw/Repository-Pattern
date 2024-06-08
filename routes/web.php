e64cd830a7a25b319c2e40eeaa868bc3049b2377
nay win <naywindb@gmail.com>
2024-06-08 20:50:10 +0630

    modify read me

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ use App\Http\Controllers\UsersController;
 // });
 
 
+Route::get('users', [UsersController::class, 'index']);
+Route::get('users/{user}', [UsersController::class, 'show']);
+Route::post('users', [UsersController::class, 'store']);
+Route::put('users/{user}', [UsersController::class, 'update']);
+Route::delete('users/{user}', [UsersController::class, 'delete']);
